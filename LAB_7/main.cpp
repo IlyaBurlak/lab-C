b5aef23d17c448fd487b128f34e2ddad821a5353
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2023-04-27 00:50:41 +0300

    NEW LAB_7

---
+++ LAB_7/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Circular_buffer/CircularBuffer.h"
+int main() {
+    CircularBuffer<int>buffer(10);
+    buffer.push_back(1);
+    buffer.push_back(2);
+    buffer.push_front(0);
+    for (size_t i = 0; i < buffer.size(); ++i) {
+        std::cout << buffer[i] << " ";
+
+    }
+    return 0;
+}
