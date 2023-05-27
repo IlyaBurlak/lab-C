a0297dfa716459b528ed650b5dc8ec98c5fc9871
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2023-05-27 10:32:53 +0300

    NEW LAB_8

--- LAB_7/main.cpp
+++ LAB_7/main.cpp
@@ -5,6 +5,9 @@ int main() {
     buffer.push_back(1);
     buffer.push_back(2);
     buffer.push_front(0);
+    buffer.capacity();
+    buffer.pop_back();
+    buffer.pop_front();
     for (size_t i = 0; i < buffer.size(); ++i) {
         std::cout << buffer[i] << " ";
 
