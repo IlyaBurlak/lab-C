f7a5594e49434da5287e3b9accf3e0a4fb5a8b87
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2023-05-27 20:12:41 +0300

    UPDATE LAB_7 std::sort

--- LAB_7/main.cpp
+++ LAB_7/main.cpp
@@ -5,9 +5,9 @@ int main() {
     buffer.push_back(1);
     buffer.push_back(2);
     buffer.push_front(0);
-    buffer.capacity();
-    buffer.pop_back();
-    buffer.pop_front();
+
+    std::sort(buffer);
+
 
     for (size_t i = 0; i < buffer.size(); ++i) {
         std::cout << buffer[i] << " ";
